e6b114272b714291a002d27fa67851358c0fc9f3
Luis González <luigolas@gmail.com>
2014-04-10 17:02:20 +0100

    Try another fix to Heroku Bootstrap problem

--- config/initializers/haml.rb
+++
@@ -1,3 +0,0 @@
-Sass::Plugin.options[:style] = :compact
-Sass::Plugin.options[:template_location] = "#{Rails.root}/public/stylesheets/sass"
-Sass::Plugin.options[:css_location] = "#{Rails.root}/tmp/public/stylesheets/compiled"
