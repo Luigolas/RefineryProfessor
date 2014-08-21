c71e268873e3f4375bcca16b46dac04be4944eaa
luigolas <luigolas@gmail.com>
2014-08-21 19:32:14 +0100

    improve responsiveness

---
+++ app/assets/javascripts/custom.js
@@ -0,0 +1,9 @@
+function checkWidth() {
+    if ($(window).width() > 992) {
+        $('#main_row').addClass('row-eq-height');
+    } else {
+        $('#main_row').removeClass('row-eq-height');
+    }
+}
+
+$(window).resize(checkWidth);
